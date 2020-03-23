556be5bc7f13af7716963c648bafdf17543b7a86
Bikash Das <das.bikash.asl@gmail.com>
2020-03-23 11:09:16 +0600

    clean up.

--- app/src/main/java/com/dasbikash/exp_man/model/TimeWiseExpenses.kt
+++
@@ -1,8 +0,0 @@
-package com.dasbikash.exp_man.model
-
-import com.dasbikash.exp_man_repo.model.ExpenseEntry
-
-data class TimeWiseExpenses(
-    val periodText:String,
-    val expenses:MutableList<ExpenseEntry> = mutableListOf()
-)
