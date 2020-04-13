c97ad3689928f1b50700bc27e1ba9013387c5f22
Bikash Das <bikashdaseee@gmail.com>
2020-04-13 17:32:31 +0600

    Data sync added on home activity once again.

--- app/src/main/java/com/dasbikash/book_keeper/activities/home/ActivityHome.kt
+++ app/src/main/java/com/dasbikash/book_keeper/activities/home/ActivityHome.kt
@@ -74,6 +74,11 @@ class ActivityHome : ActivityTemplate() {
             dataSynced.set()
             GlobalScope.launch(Dispatchers.IO) {
                 try {
+                    ExpenseRepo.syncData(this@ActivityHome)
+                    ShoppingListRepo.syncShoppingListData(this@ActivityHome)
+                    ShoppingListRepo.syncSlShareRequestData(this@ActivityHome)
+                    ConnectionRequestRepo.syncData(this@ActivityHome)
+                    AuthRepo.syncUserData(this@ActivityHome)
                     ShoppingListReminderScheduler.runReminderScheduler(this@ActivityHome)
                     debugLog("Data sync done!!")
                 } catch (ex: Throwable) {
